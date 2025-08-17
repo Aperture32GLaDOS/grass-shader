f4119df40f14f5b334c75e91d4a855306ea68212
Eshe <ehinchliffe@proton.me>
2025-08-17 18:39:09 +0100

    Added basic chunking to the grass, now frustum culling works

--- grass_compute.glsl
+++ grass_compute.glsl
@@ -2,7 +2,7 @@
 #version 450
 
 // Invocations in the (x, y, z) dimension
-layout(local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 // A binding to the buffer we create in our script
 layout(set = 0, binding = 0, std430) restrict buffer MyDataBuffer {
@@ -13,6 +13,12 @@ my_data_buffer;
 // Heightmap used to make the grass positions adhere to the terrain surface
 layout(set = 0, binding = 1) uniform sampler2D heightMap;
 
+int grassLength = 64;
+
+layout (set = 0, binding = 2) restrict uniform VecBuffer {
+  vec2 cornerPoint;
+} cornerBuffer;
+
 float rand(vec2 co){
     return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
 }
@@ -20,15 +26,17 @@ float rand(vec2 co){
 
 // The code we want to execute in each invocation
 void main() {
-    vec2 grass_position = (gl_GlobalInvocationID.xy - 512.0) / 4.0;
-    float height = length(texture(heightMap, gl_GlobalInvocationID.xy / 1024.0)) * 15.0 - 15.0;
-    uint base_index = (gl_GlobalInvocationID.y + gl_GlobalInvocationID.x * 1024) * 12;
+    vec2 grass_position = (gl_GlobalInvocationID.xy) / 8.0 + cornerBuffer.cornerPoint;
+    grass_position.x += rand(gl_GlobalInvocationID.xy) * 4.0 - 2.0;
+    grass_position.y += rand(gl_GlobalInvocationID.yx) * 4.0 - 2.0;
+    float height = length(texture(heightMap, (grass_position / (256.0 / grassLength) + (grassLength / 2.0)) / grassLength)) * 15.0 - 15.0;
+    uint base_index = (gl_GlobalInvocationID.y + gl_GlobalInvocationID.x * grassLength) * 12;
     grass_position += rand(gl_GlobalInvocationID.xy);
     // Set positions in buffer
     my_data_buffer.data[base_index] = 1;
     my_data_buffer.data[base_index + 1] = 0;
     my_data_buffer.data[base_index + 2] = 0;
-    my_data_buffer.data[base_index + 3] = grass_position.x + rand(gl_GlobalInvocationID.xy) * 2.0;
+    my_data_buffer.data[base_index + 3] = grass_position.x;
     my_data_buffer.data[base_index + 4] = 0;
     my_data_buffer.data[base_index + 5] = 1.0 + rand(gl_GlobalInvocationID.yx) * 5.0;
     my_data_buffer.data[base_index + 6] = 0;
@@ -36,5 +44,5 @@ void main() {
     my_data_buffer.data[base_index + 8] = 0;
     my_data_buffer.data[base_index + 9] = 0;
     my_data_buffer.data[base_index + 10] = 1;
-    my_data_buffer.data[base_index + 11] = grass_position.y + rand(gl_GlobalInvocationID.yx) * 2.0;
+    my_data_buffer.data[base_index + 11] = grass_position.y;
 }
--- scene.gd
+++ scene.gd
@@ -1,4 +1,7 @@
 extends Node3D
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _process(_delta: float) -> void:
 	print(Engine.get_frames_per_second())
